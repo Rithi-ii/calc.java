61c82de68b0857cb4ce8b2f212d2390a82a05c2f
Muthu Kumar N <muthukumar50006@gmail.com>
2025-02-28 11:41:28 +0530

    commited add sub

--- calc.java
+++ calc.java
@@ -1,6 +1,10 @@
-class HelloWorld {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello, World");
+public class Calc {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
     }
 }
+
